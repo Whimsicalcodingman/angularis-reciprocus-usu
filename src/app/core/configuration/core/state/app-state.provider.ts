9765784b63a2b8da471bc8e54547bfbd39ee06c7
Ims <iwhjstorm@gmail.com>
2025-08-10 11:50:54 +0200

    fix: use environmentProviders because ngrx already has a provider in itself and needs to be the correct return type

--- src/app/core/configuration/core/state/app-state.provider.ts
+++ src/app/core/configuration/core/state/app-state.provider.ts
@@ -1,10 +1,8 @@
-import { Provider } from '@angular/core';
+import { EnvironmentProviders } from '@angular/core';
 import { provideStore } from '@ngrx/store';
 
 import { STORE_CONFIG } from './store.config';
 
-export const provideAppState = (): Provider => ({
-    provide: provideStore,
-    useFactory: () => STORE_CONFIG,
-    multi: true
-})
+export const provideAppState = (): EnvironmentProviders[] => [
+  provideStore(STORE_CONFIG)
+];
